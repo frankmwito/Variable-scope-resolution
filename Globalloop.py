ee03bd2a9fdb766623c73b8ddc85bbd595646384
Frank Mwito <Mfranklyne039@gmail.com>
2024-09-13 10:47:05 +0300

    Global variable updated inside a loop

--- Globalloop.py
+++ Globalloop.py
@@ -1,1 +1,11 @@
 # function where a global variable is updated inside a loop.
+
+def global_loop():
+    for i in range(3):
+        global x
+        x += i
+        print(i)
+    
+x = 3
+
+global_loop()
